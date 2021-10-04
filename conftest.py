04d7ed588c15fa9d1dd2008684a789d326b8893b
TreySchneider95 <tschneid95@gmail.com>
2021-10-04 09:12:54 -0700

    added cron backup.txt fixed pytest

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
 import pytest
 from scrapper import *
 
-# @pytest.fixture
-# def selenium_driver():
-#     s_driver = Chrome(WEBDRIVER, options=OPTIONS)
-#     s_driver.get(URL)
-#     return s_driver
+@pytest.fixture
+def bs_driver():
+    html = requests.get(URL)
+    bs = BeautifulSoup(html.text, 'html.parser')
+    return bs
 
 
 
